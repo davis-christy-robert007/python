7ed78e4741fa295f2fc32773e2fefb4e64e38f2c
davis-christy-robert007 <daviscr2006@gmail.com>
2024-10-04 16:06:04 +0530

    Add files via upload
    python basics done in lab

---
+++ guido van sir.py
@@ -0,0 +1,41 @@
+#print("hello\nworld")
+'''
+if 5>7:
+    print("Five greater than two")
+
+if 5>2:
+       print(" five greater than two ")
+print("five greater than two")
+'''
+#x=6
+#x="dawn bhai"
+#print(x)
+
+'''
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+print(type(x))
+print((type)(y))
+print((type)(z))
+  
+'''
+'''
+x,y,z="python ","is "," awesome"
+print(x+y+z)
+username=input("enter username")
+print("username is: " +username)
+'''
+'''               
+a="hello" 
+b=" world"
+c= a + b
+print(c)
+'''
+a="hello world"
+print(a[0])
+b="Hello world"
+print(b[:11])
